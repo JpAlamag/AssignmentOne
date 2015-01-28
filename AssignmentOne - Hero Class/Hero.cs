59b6fa8a20bfc0dfb0736a8c0a548496ab3eccf4
jpalamag <jpalamag@hotmail.com>
2015-01-28 09:45:06 -0500

    Private and Public properties created

---
+++ AssignmentOne - Hero Class/Hero.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne___Hero_Class
+{
+    class Hero
+    {
+        // PRIVATE PROPERTIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        private int strength;
+        private int speed;
+        private int health;
+
+        // PUBLIC PROPERTIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public string name;
+
+        // CONSTRUCTOR ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public Hero(string name)
+        {
+            name = this.name;
+        }
+
+        // PRIVATE METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+        private void generateAbilities()
+        {
+
+        }
+
+       
+        private bool hitAttempt()
+        {
+
+        }
+
+        
+        private int hitDamage()
+        {
+
+        }
+
+
+        // PUBLIC METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public void fight()
+        {
+
+        }
+
+        public show()
+        {
+
+        }
+
+        
+        
+
+
+    }
+}
