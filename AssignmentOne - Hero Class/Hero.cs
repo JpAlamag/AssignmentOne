6dc477562af5b65cd357a75ebff336bc6b81a5eb
jpalamag <jpalamag@hotmail.com>
2015-01-30 14:58:36 -0500

    Hero Class Completed

--- AssignmentOne - Hero Class/Hero.cs
+++ AssignmentOne - Hero Class/Hero.cs
@@ -14,7 +14,9 @@ namespace AssignmentOne___Hero_Class
         private int health;
         private int hitStrength;
         private bool attempt;
-        private static Random randomNum = new Random(); // Generates random number
+
+        // Random number generator for any method that requires one
+        private static Random randomNum = new Random(); 
 
 
         // PUBLIC PROPERTIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -31,7 +33,7 @@ namespace AssignmentOne___Hero_Class
 
         
         // PRIVATE METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // random skill points generated from 0-100
+        // Random skill points generated from 0-100
         private void generateAbilities()
         {
             this.strength = randomNum.Next(0, 100) + 1;
@@ -39,21 +41,21 @@ namespace AssignmentOne___Hero_Class
             this.health = randomNum.Next(0, 100) + 1;
 
         }
-        // determines if hit is landed or not (20%)
+        // Determines if hit is landed or not (20% chance)
         private bool hitAttempt()
         {
-            if (randomNum.Next (0, 100)<=20)
-            {
-                this.attempt = true;
-            }
-            else
-            {
-                this.attempt = false;
-            }
+                if (randomNum.Next(0, 6) == 1)
+                {
+                    this.attempt = true;
+                }
+                else
+                {
+                    this.attempt = false;
+                }
             return this.attempt;
         }
 
-        // calculates amount of damage dealt to enemy (random number from 1-6)
+        // Calculates amount of damage dealt to enemy (random number from 1-6)
         private int hitDamage()
         {
             Random damage = new Random ();
@@ -63,27 +65,32 @@ namespace AssignmentOne___Hero_Class
 
 
         // PUBLIC METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Simulates combat
         public void fight()
         {
-            if (this.attempt == true)
-            {
-                hitDamage();
-                Console.WriteLine ("{0} damage dealt!", this.hitStrength);
-            }
-            else
-            {
-                Console.WriteLine("You missed! Try again, Princess ;)");
-            }
+                if (this.attempt == true)
+                {
+                    hitDamage();
+                    Console.WriteLine("{0} damage dealt! Nice hit!", this.hitStrength);
+                }
+                else
+                {
+                    Console.WriteLine("You missed! Try again, Princess ;)");
+                }
         }
 
+        // Shows heroine's randomly generated stats
         public void show()
         {
-            Console.WriteLine("|_|^|_|^|_|^|_|^_|^|_|^|_|^|_|");
-            Console.WriteLine("Princess Zelda's statistics:", this.name);
-            Console.WriteLine("Strength {0}", this.strength);
-            Console.WriteLine("Speed {0}", this.speed);
-            Console.WriteLine("health {0}", this.health);
-            Console.WriteLine("|____________________________|");
+            Console.WriteLine(" _|^|_|^|_|^|_|^|_|^|_|^|_|^|_");
+            Console.WriteLine("|                             |");
+            Console.WriteLine("|     The Hylian Heroine      |");
+            Console.WriteLine("|       Princess {0}        |", this.name);
+            Console.WriteLine("|                             |");
+            Console.WriteLine("| Strength: {0}", this.strength);
+            Console.WriteLine("| Speed: {0}", this.speed);
+            Console.WriteLine("| Health: {0}", this.health);
+            Console.WriteLine("|_____________________________|");
         }
 
         
--- AssignmentOne - Hero Class/Program.cs
+++ AssignmentOne - Hero Class/Program.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Name: John Paul Alamag - 300792027
+ * Program: Assignment One - Hero Class
+ * Date last modified: 1/29/2015
+ * Revision History:
+ */
 namespace AssignmentOne___Hero_Class
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Implemented Hero class
+            Hero myhero = new Hero("Zelda");
+            myhero.show();
+            Console.WriteLine();
+            myhero.fight();
+
+
         }
     }
 }
